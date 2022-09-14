55a049a42880e591230e9a21a9e7e2db415851c1
FadFlex <fsiddiqui107@gmail.com>
2022-09-14 10:26:58 +0500

    work

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,8 +2,10 @@ import { ArrowDropDown } from "@material-ui/icons";
 import React from "react";
 import Recommendation from "../recommendation/Recommendation";
 import "./rightbar.css";
+import { useSelector } from "react-redux";
 
 export default function Rightbar() {
+  const name = useSelector((state)=>state.user.name)
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
