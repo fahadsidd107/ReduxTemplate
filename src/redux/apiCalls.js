2593d7837a65e6a053ebc3066f3a8a701d210cd9
FadFlex <fsiddiqui107@gmail.com>
2022-09-15 10:22:51 +0500

    work

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -4,7 +4,7 @@ import { updateStart, updateSuccess, updateFailure } from "./userSlice";
 export const updateUser = async (user, dispatch) => {
   dispatch(updateStart());
   try {
-    const res = await axios.post("http://localhost:8800/api/users/1/update", user);
+    const res = await axios.post("YOUR API", user);
     dispatch(updateSuccess(res.data));
   } catch (err) {
     dispatch(updateFailure());
